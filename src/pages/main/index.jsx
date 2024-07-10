ea089c419620692628dd2b13effd62a2a9451043
lucy2n <lyusianaumenko@yandex.by>
2024-07-10 16:43:43 +0300

    feat: add basic projects-block

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,33 +3,15 @@ import Header from '../../components/header';
 import About from '../../components/about';
 import style from './style.module.css';
 import Info from '../../components/info';
+import Projects from '../../components/projects';
 
 const MainPage = () => {
-    const [isInView, setIsInView] = useState(false);
-    const targetRef = useRef(null);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        if (targetRef.current) {
-          const rect = targetRef.current.getBoundingClientRect();
-          const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
-          setIsInView(isVisible);
-        }
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-      handleScroll();
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-
     return (
         <div>
             <Header />
-            <About ref={targetRef} className={isInView ? style.image_block_active : ''} />
+            <About />
             <Info />
+            <Projects />
         </div>
     );
 };
